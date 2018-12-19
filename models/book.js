3d6551733b38dced8dafc899afabe9e15e364e2f
Sicimike <877434251@qq.com>
2018-12-19 17:44:39 +0800

    book detail page

--- models/book.js
+++ models/book.js
@@ -7,7 +7,25 @@ class BookModel extends HTTP {
     getHotList() {
         return this.request({
             url: 'book/hot/list'
-        })
+        });
+    }
+
+    getDetail(bid) {
+        return this.request({
+            url: '/book/' + bid + '/detail'
+        });
+    }
+
+    getLikeStatus(bid) {
+        return this.request({
+            url: '/book/' + bid + '/favor'
+        });
+    }
+
+    getComment(bid) {
+        return this.request({
+            url: '/book/' + bid + '/short/comment'
+        });
     }
 }
 
